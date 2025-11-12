ac1d06fc21d5d3deb2f482fd4fe9381b46215bc0
Tomkoid <tomaszierl@outlook.com>
2025-11-12 20:43:23 +0100

    chore: use serialize generic for serializing structs that don't implement serde::Serialize
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- src/error.rs
+++ src/error.rs
@@ -7,15 +7,15 @@ pub enum TidalError {
     NotAuthenticated,
 
     #[error("HTTP request failed: {0}")]
-    #[serde(serialize_with = "serialize_reqwest_error")]
+    #[serde(serialize_with = "serialize_generic")]
     Request(#[from] reqwest::Error),
 
     #[error("failed to parse JSON response: {0}")]
-    #[serde(serialize_with = "serialize_json_error")]
+    #[serde(serialize_with = "serialize_generic")]
     JsonParse(#[from] serde_json::Error),
 
     #[error("request client error: {0}")]
-    #[serde(serialize_with = "serialize_request_client_error")]
+    #[serde(serialize_with = "serialize_generic")]
     RequestClient(#[from] crate::requests::RequestClientError),
 
     #[error("authentication failed: {0}")]
@@ -31,67 +31,22 @@ pub enum TidalError {
     Logout(String),
 
     #[error("failed to parse integer: {0}")]
-    #[serde(serialize_with = "serialize_parse_int_error")]
+    #[serde(serialize_with = "serialize_generic")]
     ParseIntError(#[from] std::num::ParseIntError),
 
     #[error("base64 decode error: {0}")]
-    #[serde(serialize_with = "serialize_base64_error")]
+    #[serde(serialize_with = "serialize_generic")]
     Base64DecodeError(#[from] base64::DecodeError),
 
     #[error("string from utf8 error: {0}")]
-    #[serde(serialize_with = "serialize_from_utf8_error")]
+    #[serde(serialize_with = "serialize_generic")]
     StringFromUTF8Error(#[from] std::string::FromUtf8Error),
 
     #[error("{0}")]
     Other(String),
 }
 
-// Helper functions to serialize non-serializable error types
-fn serialize_reqwest_error<S>(error: &reqwest::Error, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: serde::Serializer,
-{
-    serializer.serialize_str(&error.to_string())
-}
-
-fn serialize_json_error<S>(error: &serde_json::Error, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: serde::Serializer,
-{
-    serializer.serialize_str(&error.to_string())
-}
-
-fn serialize_base64_error<S>(error: &base64::DecodeError, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: serde::Serializer,
-{
-    serializer.serialize_str(&error.to_string())
-}
-
-fn serialize_request_client_error<S>(
-    error: &crate::requests::RequestClientError,
-    serializer: S,
-) -> Result<S::Ok, S::Error>
-where
-    S: serde::Serializer,
-{
-    serializer.serialize_str(&error.to_string())
-}
-
-fn serialize_parse_int_error<S>(
-    error: &std::num::ParseIntError,
-    serializer: S,
-) -> Result<S::Ok, S::Error>
-where
-    S: serde::Serializer,
-{
-    serializer.serialize_str(&error.to_string())
-}
-
-fn serialize_from_utf8_error<S>(
-    error: &std::string::FromUtf8Error,
-    serializer: S,
-) -> Result<S::Ok, S::Error>
+fn serialize_generic<S>(error: &dyn std::error::Error, serializer: S) -> Result<S::Ok, S::Error>
 where
     S: serde::Serializer,
 {
