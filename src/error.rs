4e3f2ddb3448baf18d0d395658b97c7cecd6f380
Tomkoid <tomaszierl@outlook.com>
2025-11-12 18:08:05 +0100

    chore: impl Serialize on TidalError
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- src/error.rs
+++ src/error.rs
@@ -1,17 +1,21 @@
+use serde::Serialize;
 use thiserror::Error;
 
-#[derive(Error, Debug)]
+#[derive(Error, Debug, Serialize)]
 pub enum TidalError {
     #[error("not authenticated, either session and/or page doesn't have access token set")]
     NotAuthenticated,
 
     #[error("HTTP request failed: {0}")]
+    #[serde(serialize_with = "serialize_reqwest_error")]
     Request(#[from] reqwest::Error),
 
     #[error("failed to parse JSON response: {0}")]
+    #[serde(serialize_with = "serialize_json_error")]
     JsonParse(#[from] serde_json::Error),
 
     #[error("request client error: {0}")]
+    #[serde(serialize_with = "serialize_request_client_error")]
     RequestClient(#[from] crate::requests::RequestClientError),
 
     #[error("authentication failed: {0}")]
@@ -27,14 +31,69 @@ pub enum TidalError {
     Logout(String),
 
     #[error("failed to parse integer: {0}")]
+    #[serde(serialize_with = "serialize_parse_int_error")]
     ParseIntError(#[from] std::num::ParseIntError),
 
     #[error("base64 decode error: {0}")]
+    #[serde(serialize_with = "serialize_base64_error")]
     Base64DecodeError(#[from] base64::DecodeError),
 
     #[error("string from utf8 error: {0}")]
+    #[serde(serialize_with = "serialize_from_utf8_error")]
     StringFromUTF8Error(#[from] std::string::FromUtf8Error),
 
     #[error("{0}")]
     Other(String),
+}
+
+// Helper functions to serialize non-serializable error types
+fn serialize_reqwest_error<S>(error: &reqwest::Error, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    serializer.serialize_str(&error.to_string())
+}
+
+fn serialize_json_error<S>(error: &serde_json::Error, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    serializer.serialize_str(&error.to_string())
+}
+
+fn serialize_base64_error<S>(error: &base64::DecodeError, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    serializer.serialize_str(&error.to_string())
+}
+
+fn serialize_request_client_error<S>(
+    error: &crate::requests::RequestClientError,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    serializer.serialize_str(&error.to_string())
+}
+
+fn serialize_parse_int_error<S>(
+    error: &std::num::ParseIntError,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    serializer.serialize_str(&error.to_string())
+}
+
+fn serialize_from_utf8_error<S>(
+    error: &std::string::FromUtf8Error,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    serializer.serialize_str(&error.to_string())
 }
