1bb17c68e02e483db55f39bdd54810a5215af698
Tomkoid <tomaszierl@outlook.com>
2025-12-07 21:02:30 +0100

    feat: add create playlist and create folder functionality
    Signed-off-by: Tomkoid <tomaszierl@outlook.com>

--- src/client/api/collection.rs
+++ src/client/api/collection.rs
@@ -1,4 +1,10 @@
-use crate::client::TidalClient;
+use crate::client::{
+    TidalClient,
+    models::{
+        collection::{FolderCollectionItem, PlaylistCollectionItem, SharingLevel},
+        playlist::PlaylistInfo,
+    },
+};
 use std::collections::HashMap;
 
 use crate::{
@@ -9,6 +15,73 @@ use crate::{
 };
 
 impl TidalClient {
+    pub async fn create_playlist(
+        &mut self,
+        title: impl Into<String>,
+        description: impl Into<String>,
+        sharing_level: Option<SharingLevel>,
+        parent_id: Option<String>,
+    ) -> Result<PlaylistCollectionItem, TidalError> {
+        let url = format!("/my-collection/playlists/folders/create-playlist");
+
+        let mut req = TidalRequest::new(reqwest::Method::GET, url.clone());
+        let mut params = HashMap::new();
+        params.insert(
+            "countryCode".to_string(),
+            self.user_info.as_ref().unwrap().country_code.clone(),
+        );
+        params.insert("name".to_string(), title.into());
+        params.insert("description".to_string(), description.into());
+        params.insert(
+            "folderId".to_string(),
+            parent_id.unwrap_or("root".to_string()),
+        );
+        params.insert(
+            "isPublic".to_string(),
+            (sharing_level.unwrap_or(SharingLevel::Private) == SharingLevel::Public).to_string(),
+        );
+
+        req.method = reqwest::Method::PUT;
+        req.params = Some(params);
+        req.access_token = self.session.auth.access_token.clone();
+        req.base_url = Some(Self::API_V2_LOCATION.to_string());
+
+        let resp = self.rq.request(req).await?;
+        let body = resp.text().await?;
+
+        Ok(serde_json::from_str(&body)?)
+    }
+
+    pub async fn create_folder(
+        &mut self,
+        title: impl Into<String>,
+        parent_id: Option<String>,
+    ) -> Result<FolderCollectionItem, TidalError> {
+        let url = format!("/my-collection/playlists/folders/create-folder");
+
+        let mut req = TidalRequest::new(reqwest::Method::GET, url.clone());
+        let mut params = HashMap::new();
+        params.insert(
+            "countryCode".to_string(),
+            self.user_info.as_ref().unwrap().country_code.clone(),
+        );
+        params.insert("name".to_string(), title.into());
+        params.insert(
+            "folderId".to_string(),
+            parent_id.unwrap_or("root".to_string()),
+        );
+
+        req.method = reqwest::Method::PUT;
+        req.params = Some(params);
+        req.access_token = self.session.auth.access_token.clone();
+        req.base_url = Some(Self::API_V2_LOCATION.to_string());
+
+        let resp = self.rq.request(req).await?;
+        let body = resp.text().await?;
+
+        Ok(serde_json::from_str(&body)?)
+    }
+
     pub async fn get_collection_artists(
         &mut self,
         limit: u32,
--- src/client/models/collection.rs
+++ src/client/models/collection.rs
@@ -77,3 +77,84 @@ pub struct CollectionArtistRole {
     pub category_id: i64,
     pub category: String,
 }
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PlaylistCollectionItem {
+    pub trn: String,
+    pub item_type: String,
+    pub added_at: String,
+    pub last_modified_at: String,
+    pub name: String,
+    pub parent: Option<String>,
+    pub data: PlaylistData,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PlaylistData {
+    pub uuid: String,
+    #[serde(rename = "type")]
+    pub playlist_type: String,
+    pub creator: Creator,
+    pub curators: Vec<serde_json::Value>,
+    pub content_behavior: String,
+    pub sharing_level: String,
+    pub status: String,
+    pub source: String,
+    pub title: String,
+    pub description: String,
+    pub image: String,
+    pub square_image: String,
+    pub custom_image_url: Option<String>,
+    pub url: String,
+    pub created: String,
+    pub last_updated: String,
+    pub last_item_added_at: Option<String>,
+    pub duration: i64,
+    pub number_of_tracks: i64,
+    pub number_of_videos: i64,
+    pub promoted_artists: Vec<serde_json::Value>,
+    pub trn: String,
+    pub item_type: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Creator {
+    pub id: i64,
+    pub name: Option<String>,
+    pub picture: Option<String>,
+    #[serde(rename = "type")]
+    pub creator_type: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub enum SharingLevel {
+    Public,
+    Private,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct FolderCollectionItem {
+    pub added_at: String,
+    pub data: FolderData,
+    pub item_type: String,
+    pub last_modified_at: String,
+    pub name: String,
+    pub parent: Option<String>,
+    pub trn: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct FolderData {
+    pub created_at: String,
+    pub id: String,
+    pub item_type: String,
+    pub last_modified_at: String,
+    pub name: String,
+    pub total_number_of_items: i64,
+    pub trn: String,
+}
