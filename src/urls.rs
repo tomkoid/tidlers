5577756672ab1f972f6552fcb372567694c47622
Tomkoid <tomkoid@tomkoid.cz>
2026-04-29 22:28:51 +0200

    chore(urls): apply clippy suggestions

--- src/urls.rs
+++ src/urls.rs
@@ -1,4 +1,4 @@
-pub const API_V1_LOCATION: &'static str = "https://api.tidal.com/v1";
-pub const API_V2_LOCATION: &'static str = "https://api.tidal.com/v2";
-pub const OPEN_API_V2_LOCATION: &'static str = "https://openapi.tidal.com/v2";
-pub const WEB_API_V2_LOCATION: &'static str = "https://tidal.com/v2";
+pub const API_V1_LOCATION: &str = "https://api.tidal.com/v1";
+pub const API_V2_LOCATION: &str = "https://api.tidal.com/v2";
+pub const OPEN_API_V2_LOCATION: &str = "https://openapi.tidal.com/v2";
+pub const WEB_API_V2_LOCATION: &str = "https://tidal.com/v2";
