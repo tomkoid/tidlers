4c15eedb6f1b14d23c3fef2e0895524f2026f03a
Tomkoid <tomkoid@tomkoid.cz>
2026-05-07 19:58:57 +0200

    chore(collection): move favorite track and album into their own mods

--- src/client/api/collection/mod.rs
+++ src/client/api/collection/mod.rs
@@ -1,4 +1,6 @@
+pub mod album;
 pub mod artist;
 pub mod favorites;
 pub mod folder;
 pub mod playlist;
+pub mod track;
---
+++ src/client/api/collection/track.rs
@@ -0,0 +1,28 @@
+use crate::{
+    TidalClient, TidalError, client::models::collection::track::CollectionFavoriteTracksResponse,
+    urls::API_V1_LOCATION,
+};
+
+impl TidalClient {
+    /// Retrieves the user's favorite tracks with optional limit (default 100) and offset (default
+    /// 0)
+    pub async fn get_collection_track_favorites(
+        &self,
+        limit: Option<u32>,
+        offset: Option<u32>,
+    ) -> Result<CollectionFavoriteTracksResponse, TidalError> {
+        let url = format!("/users/{}/favorites/tracks", self.user_id()?);
+
+        let body = self
+            .request(reqwest::Method::GET, url)
+            .with_country_code()
+            .with_locale()
+            .with_param("limit", limit.unwrap_or(100).to_string())
+            .with_param("ofset", offset.unwrap_or(0).to_string())
+            .with_base_url(API_V1_LOCATION)
+            .send()
+            .await?;
+
+        Ok(body)
+    }
+}
