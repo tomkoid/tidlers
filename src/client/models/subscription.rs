4b4273bb2e40d7b738887733b59c59575f6ffcfe
Tomkoid <tomkoid@tomkoid.cz>
2026-07-15 14:35:37 +0200

    chore(subscription): cleanup model

--- src/client/models/subscription.rs
+++ src/client/models/subscription.rs
@@ -1,27 +1,21 @@
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct UserSubscriptionResponse {
-    #[serde(rename = "startDate")]
     pub start_date: String,
-    #[serde(rename = "validUntil")]
     pub valid_until: String,
     pub status: String,
     pub subscription: SubscriptionPlan,
-    #[serde(rename = "highestSoundQuality")]
     pub highest_sound_quality: String,
-    #[serde(rename = "premiumAccess")]
     pub premium_access: bool,
-    #[serde(rename = "canGetTrial")]
     pub can_get_trial: bool,
-    #[serde(rename = "paymentType")]
     pub payment_type: String,
-    #[serde(rename = "paymentOverdue")]
     pub payment_overdue: bool,
 }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct SubscriptionPlan {
     #[serde(rename = "type")]
     pub subscription_type: String,
-    #[serde(rename = "offlineGracePeriod")]
     pub offline_grace_period: u32,
 }
