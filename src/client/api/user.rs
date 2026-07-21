a390d33b3236962033be964ce142b6bb12e52ca7
Tomkoid <tomkoid@tomkoid.cz>
2026-07-21 18:32:40 +0200

    chore(api): move get_user_info(), refresh_user_info(), logout() code to user.rs

--- src/client/api/user.rs
+++ src/client/api/user.rs
@@ -1,7 +1,9 @@
 use crate::{
     TidalClient, TidalError,
-    client::models::user::{UserV1Response, UserV2Response},
-    urls::API_V2_LOCATION,
+    client::models::user::{User, UserProfileResource, UserV1Response, UserV2Response},
+    requests::TidalRequest,
+    responses::ApiDataResponse,
+    urls::{API_V2_LOCATION, OPEN_API_V2_LOCATION},
 };
 
 impl TidalClient {
@@ -22,5 +24,52 @@ impl TidalClient {
             .with_base_url(API_V2_LOCATION)
             .send()
             .await
+    }
+
+    /// Returns the current user's information, optionally forcing a refresh from the API
+    pub async fn get_user_info(&mut self) -> Result<UserProfileResource, TidalError> {
+        let json: ApiDataResponse<UserProfileResource> = self
+            .request(reqwest::Method::GET, "/users/me")
+            .with_base_url(OPEN_API_V2_LOCATION)
+            .send()
+            .await?;
+
+        Ok(json.data)
+    }
+
+    /// Refreshes the internally stored user information by fetching it from the API, this is needed
+    /// after login or if you want to make sure you have the latest info that is then used for some
+    /// other functionality
+    pub async fn refresh_user_info(&mut self) -> Result<(), TidalError> {
+        let ui = self.get_user_info().await?;
+
+        self.user_info = Some(User {
+            user_id: ui.id.parse()?,
+            username: ui.attributes.username,
+            email: ui.attributes.email,
+            country_code: ui.attributes.country,
+            email_verified: ui.attributes.email_verified,
+
+            ..self.user_info.clone().unwrap()
+        });
+
+        Ok(())
+    }
+
+    /// Logs the current user out of Tidal.
+    pub async fn logout(&self) -> Result<(), TidalError> {
+        let url = "/logout".to_string();
+
+        let mut req = TidalRequest::new(reqwest::Method::POST, url.clone());
+        req.access_token = self.session.auth.access_token.clone();
+
+        let resp = self.rq.request(req).await?;
+        let status = resp.status();
+
+        if status != reqwest::StatusCode::NO_CONTENT {
+            return Err(TidalError::Logout(status.as_str().to_string()));
+        }
+
+        Ok(())
     }
 }
